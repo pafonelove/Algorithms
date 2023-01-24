b45e44d797c5c9e6fb1d3f097658a35e97d8a3ab
Bearded Brain <58626500+pafonelove@users.noreply.github.com>
2023-01-24 14:08:02 +0400

    24.01.23
    Added Stack class for Stack working demonstration

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Algorithms {
+    class MyClass {
+        static void Main() {
+            // Программа, демонстрирующая базовую работу стека.
+            Stack ob1 = new Stack(5);   // создание экземпляра класса
+            
+            // Заполнение стек значениями.
+            for (int i = 0, j = 10; i < ob1.arr.Length; i++, j*=2) {
+                ob1.Push(j);
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Количество элементо в стеке: {ob1.Count()}");
+
+            // Попытка вставить значение в заполненный стек.
+            ob1.Push(15);
+
+            Console.Write("Значения из стека: ");
+            // Выборка значений из стека.
+            for (int i = 0; i < ob1.arr.Length; i++) {
+                Console.Write($"{ob1.Pop()} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Количество элементо в стеке: {ob1.Count()}");
+
+            // Попытка взять значение из пустого стека.
+            ob1.Pop();
+        }
+    }
+}
